fb0cbd1b0a8b55e29fcba03cbbb05455908f7c4d
JoeCalvi <joecalvi@me.com>
2023-06-14 15:34:10 -0600

    why man

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = {
     entry: './index.ts',
     target: 'node',
+    mode: 'production',
     module: {
         rules: [
             {
@@ -13,7 +14,7 @@ module.exports = {
         ],
     },
     resolve: {
-        extensions: ['.ts', '.js'],
+        extensions: ['.tsx', '.ts', '.js'],
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
