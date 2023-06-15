a99c4fa65072ab23e6b3eeec13327a44173065fa
JoeCalvi <joecalvi@me.com>
2023-06-15 08:06:21 -0600

    how about this

--- client/Axios.js
+++ client/Axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: 'http://localhost:3080/api',
+    baseURL: 'https://riverrockroofing.onrender.com/api',
     timeout: 10000
 });
