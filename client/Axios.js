2f32daec6b359386e37011b62eed9ff5d3ffac6a
JoeCalvi <joecalvi@me.com>
2023-06-20 17:37:23 -0600

    adjusted api pointer

--- client/Axios.js
+++ client/Axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: 'https://riverrockroofing.onrender.com/api',
+    baseURL: 'https://www.riverrockroofingid.com/api',
     timeout: 10000
 });
