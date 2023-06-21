7fcac06f6d5a3fd3010156a2ea71bfbd753683e7
JoeCalvi <joecalvi@me.com>
2023-06-21 15:48:55 -0600

    added photos to gallery

--- client/src/views/GalleryView.vue
+++ client/src/views/GalleryView.vue
@@ -8,19 +8,34 @@
       <div class="col-12 jet-bg-black">
         <section class="masonry my-3">
           <div class="brick mb-3">
+            <img src="../assets/images/roof2june21.jpeg" class="w-100 rounded" alt="Before Image of Roof">
+          </div>
+          <div class="brick mb-3">
+            <img src="../assets/images/roof3june21.jpeg" class="w-100 rounded" alt="Roof During Job">
+          </div>
+          <div class="brick mb-3">
             <img src="../assets/images/gallery9.jpg" class="w-100 rounded" alt="Example Roof">
           </div>
           <div class="brick mb-3">
             <img src="../assets/images/gallery1.jpg" class="w-100 rounded" alt="Example Roof">
           </div>
           <div class="brick mb-3">
+            <img src="../assets/images/gallery13.jpg" class="w-100 rounded" alt="Example Roof">
+          </div>
+          <div class="brick mb-3">
             <img src="../assets/images/gallery5.jpg" class="w-100 rounded" alt="Example Roof">
           </div>
           <div class="brick mb-3">
-            <img src="../assets/images/gallery7.jpg" class="w-100 rounded" alt="Example Roof">
+            <img src="../assets/images/gallery15.jpg" class="w-100 rounded" alt="Example Roof">
           </div>
           <div class="brick mb-3">
-            <img src="../assets/images/gallery15.jpg" class="w-100 rounded" alt="Example Roof">
+            <img src="../assets/images/gallery14.jpg" class="w-100 rounded" alt="Example Roof">
+          </div>
+          <div class="brick mb-3">
+            <img src="../assets/images/roofjune21.jpeg" class="w-100 rounded" alt="Example Roof">
+          </div>
+          <div class="brick mb-3">
+            <img src="../assets/images/gallery7.jpg" class="w-100 rounded" alt="Example Roof">
           </div>
           <div class="brick mb-3">
             <img src="../assets/images/gallery3.jpg" class="w-100 rounded" alt="Example Roof">
@@ -29,7 +44,7 @@
             <img src="../assets/images/gallery6.jpg" class="w-100 rounded" alt="Example Roof">
           </div>
           <div class="brick mb-3">
-            <img src="../assets/images/gallery14.jpg" class="w-100 rounded" alt="Example Roof">
+            <img src="../assets/images/roof4june21.jpeg" class="w-100 rounded" alt="Example Roof">
           </div>
           <div class="brick mb-3">
             <img src="../assets/images/gallery8.jpg" class="w-100 rounded" alt="Example Roof">
@@ -42,12 +57,6 @@
           </div>
           <div class="brick mb-3">
             <img src="../assets/images/gallery11.jpg" class="w-100 rounded" alt="Example Roof">
-          </div>
-          <div class="brick mb-3">
-            <img src="../assets/images/gallery12.jpg" class="w-100 rounded" alt="Example Roof">
-          </div>
-          <div class="brick mb-3">
-            <img src="../assets/images/gallery13.jpg" class="w-100 rounded" alt="Example Roof">
           </div>
         </section>
       </div>
